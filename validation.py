de28f14ae0bda2b6f54de45c3da6807dd28c005d
vadim.kaplanov <freeman123>
2018-11-01 09:52:21 +0300

    feature selection

--- validation.py
+++ validation.py
@@ -10,10 +10,10 @@ _DATA_PATH = 'data/'
 _SAMPLE = 10000
 
 data_sets = [
-    # 'check_1_r',
-    # 'check_2_r',
-    # 'check_3_r',
-    # 'check_4_c', 'check_5_c', 'check_6_c',
+    'check_1_r',
+    'check_2_r',
+    'check_3_r',
+    'check_4_c', 'check_5_c', 'check_6_c',
     'check_7_c',
     'check_8_c'
 ]
